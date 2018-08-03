f1c1c9f2948f27536d008e4ce853a80bde8df554
watal <watal.i27e@gmai.com>
2018-08-03 19:45:16 +0900

    Add Regex to simple conversation

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // モジュールのインポート
 const line = require('@line/bot-sdk');  // Messaging APIのSDK
 const express = require('express')();    // Express
@@ -39,24 +37,38 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
     const pedigree_quiz = [pedigree_quiz_2013, pedigree_quiz_2014, pedigree_quiz_2015, pedigree_quiz_2016];
     const Dam_data = require('./data/json_data/Dam.json');
 
-    // イベントオブジェクトを順次処理
+    // イベントを順次処理
     req.body.events.forEach((event) => {
         // イベントタイプごとに処理を決定
         switch (event.type) {
             case 'message':
-                const message = event.message;
+                let message = event.message;
                 switch (message.type) {
                     // テキストメッセージに返信
                     case 'text':
                         // 1対1の会話パターン
                         for (let i in cnv_pattern) {
-                            if (message.text == cnv_pattern[i][0]) {
+                            let match_pattern;
+                            if (cnv_pattern[i][0] == 0) {
+                            // 配列の第0要素が1なら完全一致
+                                match_pattern = new RegExp('^' + cnv_pattern[i][1] + '$');
+                            // 配列の第0要素が1なら部分一致
+                            } else if (cnv_pattern[i][0] == 1) {
+                                match_pattern = new RegExp('.*' + cnv_pattern[i][1] + '.*');
+                            // 配列の第0要素が2なら前方一致
+                            } else if (cnv_pattern[i][0] == 2) {
+                                match_pattern = new RegExp('^' + cnv_pattern[i][1] + '.*');
+                            // 配列の第0要素が3なら後方一致
+                            } else if (cnv_pattern[i][0] == 3) {
+                                match_pattern = new RegExp('.*' + cnv_pattern[i][1] + '$');
+                            };
+                            if (message.text.match(match_pattern)) {
                                 bot.replyMessage(event.replyToken, {
                                     type: 'text',
-                                    text: cnv_pattern[i][1]
+                                    text: cnv_pattern[i][2]
                                 });
-                            }
-                        }
+                            };
+                        };
                         // クイズ  take_quiz(message);
                         if (message.text == '血統クイズ') {
                             bot.pushMessage(event.source.groupId, {
@@ -65,17 +77,17 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                             });
                             // 年度を選ぶ乱数
                             let quiz_year = Math.floor( Math.random() * 4 );
-                            let true_year = quiz_year + 2013
+                            let true_year = quiz_year + 2013;
                             // 要素を選ぶ乱数 □地・の20XX系と獲得賞金0万円は飛ばす
                             while(true) {
                                 var random = Math.floor( Math.random() * pedigree_quiz[quiz_year].length );
-                                var target_year = pedigree_quiz[quiz_year][random]
+                                var target_year = pedigree_quiz[quiz_year][random];
                                 let name_key = 'の' + true_year;
-                                let name_string = target_year[1].name
+                                let name_string = target_year[1].name;
                                 if (name_string.indexOf('□地') && !((name_string.lastIndexOf(name_key) + name_key.length === name_string.length) && (name_key.length<=name_string.length)) && target_year[3].money != '0万円' && target_year[3].money != '-') {
                                     break;
-                                }
-                            }
+                                };
+                            };
                             // 10秒に1回ヒントを提出
                             setTimeout(() => {
                                 bot.pushMessage(event.source.groupId, {
@@ -107,7 +119,7 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                     }, 6000);
                                 }, 6000);
                             }, 1000);
-                        }
+                        };
 
                         if (message.text == '戦績クイズ') {
                             bot.pushMessage(event.source.groupId, {
@@ -116,24 +128,24 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                             });
                             // 年度を選ぶ乱数
                             let quiz_year = Math.floor( Math.random() * 4 );
-                            let true_year = quiz_year + 2013
+                            let true_year = quiz_year + 2013;
                             // 要素を選ぶ乱数 □地・○地・の20XX系と獲得賞金0万円は飛ばす
                             while(true) {
                                 var random = Math.floor( Math.random() * pedigree_quiz[quiz_year].length );
-                                var target_year = pedigree_quiz[quiz_year][random]
+                                var target_year = pedigree_quiz[quiz_year][random];
                                 let name_key = 'の' + true_year;
-                                let name_string = target_year[1].name
+                                let name_string = target_year[1].name;
                                 if (name_string.indexOf('□地') && name_string.indexOf('○地') &&!((name_string.lastIndexOf(name_key) + name_key.length === name_string.length) && (name_key.length<=name_string.length)) && target_year[3].money != '0万円' && target_year[3].money != '-' && target_year[7].length >= 6) {
                                     break;
-                                }
-                            }
+                                };
+                            };
 
                             // 5走前，4走前，3走前，2走前の待ち時間
-                            let wait_times = [6000, 6000, 6000, 6000]
-                            if (target_year[7][24] == null) {wait_times[0] = 3000}
-                            if (target_year[7][18] == null) {wait_times[1] = 3000}
-                            if (target_year[7][12] == null) {wait_times[2] = 3000}
-                            if (target_year[7][6] == null) {wait_times[3] = 3000}
+                            const wait_times = [6000, 6000, 6000, 6000];
+                            if (target_year[7][24] == null) {wait_times[0] = 3000};
+                            if (target_year[7][18] == null) {wait_times[1] = 3000};
+                            if (target_year[7][12] == null) {wait_times[2] = 3000};
+                            if (target_year[7][6] == null) {wait_times[3] = 3000};
 
                             // 10秒に1回ヒントを提出
                             setTimeout(() => {
@@ -141,37 +153,37 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                     bot.pushMessage(event.source.groupId, {
                                         type: 'text',
                                         text: '5走前：\n　日付：' + target_year[7][24].race_ymd + '\n　開催：' + target_year[7][25].race_place + '\n　名称：' + target_year[7][26].race_name + '\n　着順：' + target_year[7][27].race_result + '着\n　鞍上：' + target_year[7][28].race_jockey + '\n　条件：' + target_year[7][29].race_conditions
-                                    })
+                                    });
                                 } else {
                                     bot.pushMessage(event.source.groupId, {
                                         type: 'text',
                                         text: '5走前：なし'
                                     });
-                                }
+                                };
                                 setTimeout(() => {
                                     if (target_year[7][18] != null) {
                                         bot.pushMessage(event.source.groupId, {
                                             type: 'text',
                                             text: '4走前：\n　日付：' + target_year[7][18].race_ymd + '\n　開催：' + target_year[7][19].race_place + '\n　名称：' + target_year[7][20].race_name + '\n　着順：' + target_year[7][21].race_result + '着\n　鞍上：' + target_year[7][22].race_jockey + '\n　条件：' + target_year[7][23].race_conditions
-                                        })
+                                        });
                                     } else {
                                         bot.pushMessage(event.source.groupId, {
                                             type: 'text',
                                             text: '4走前：なし'
                                         });
-                                    }
+                                    };
                                     setTimeout(() => {
                                         if (target_year[7][12] != null) {
                                             bot.pushMessage(event.source.groupId, {
                                                 type: 'text',
                                                 text: '3走前：\n　日付：' + target_year[7][12].race_ymd + '\n　開催：' + target_year[7][13].race_place + '\n　名称：' + target_year[7][14].race_name + '\n　着順：' + target_year[7][15].race_result + '着\n　鞍上：' + target_year[7][16].race_jockey + '\n　条件：' + target_year[7][17].race_conditions
-                                            })
+                                            });
                                         } else {
                                             bot.pushMessage(event.source.groupId, {
                                                 type: 'text',
                                                 text: '3走前：なし'
                                             });
-                                        }
+                                        };
                                             setTimeout(() => {
                                             if (target_year[7][6] != null) {
                                                 bot.pushMessage(event.source.groupId, {
@@ -183,7 +195,7 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                                     type: 'text',
                                                     text: '2走前：なし'
                                                 });
-                                            }
+                                            };
                                             setTimeout(() => {
                                                 bot.pushMessage(event.source.groupId, {
                                                     type: 'text',
@@ -200,7 +212,7 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                     }, wait_times[1]);
                                 }, wait_times[0]);
                             }, 1000);
-                        }
+                        };
 
                         if (message.text == 'ダムクイズ') {
                             bot.pushMessage(event.source.groupId, {
@@ -208,27 +220,27 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                 text: 'ダムクイズ！張り切っていきましょー！'
                             });
 
-                            const dam_purpose = ['洪水調整，農地防災', '不特定用水，河川維持用水', '灌漑，特定（新規）灌漑用水', '上水道用水', '工業用水道用水', '発電', '消流雪用水', 'レクリエーション']
-                            const dam_type = ['アーチダム', 'バットレスダム', 'アースダム', 'アスファルトフェイシングダム', 'アスファルトコアダム', 'フローティングゲートダム', '重力式コンクリートダム', '重力式アーチダム', '重力式コンクリートダム・フィルダム複合ダム', '中空重力式コンクリートダム', 'マルティプルアーチダム', 'ロックフィルダム', '台形CSGダム']
+                            const dam_purpose = ['洪水調整，農地防災', '不特定用水，河川維持用水', '灌漑，特定（新規）灌漑用水', '上水道用水', '工業用水道用水', '発電', '消流雪用水', 'レクリエーション'];
+                            const dam_type = ['アーチダム', 'バットレスダム', 'アースダム', 'アスファルトフェイシングダム', 'アスファルトコアダム', 'フローティングゲートダム', '重力式コンクリートダム', '重力式アーチダム', '重力式コンクリートダム・フィルダム複合ダム', '中空重力式コンクリートダム', 'マルティプルアーチダム', 'ロックフィルダム', '台形CSGダム'];
 
                             // ダムを選ぶ乱数
                             while (true) {
                                 var random = Math.floor( Math.random() * Dam_data['ksj:Dataset']['ksj:Dam'].length);
-                                var target_dam = Dam_data['ksj:Dataset']['ksj:Dam'][random]
+                                var target_dam = Dam_data['ksj:Dataset']['ksj:Dam'][random];
                                 break;
-                            }
+                            };
 
-                            let purposes = target_dam['ksj:purpose']
+                            let purposes = target_dam['ksj:purpose'];
                             if (typeof purposes === 'object') {
-                                target_purpose = dam_purpose[target_dam['ksj:purpose'][0] - 1]
+                                target_purpose = dam_purpose[target_dam['ksj:purpose'][0] - 1];
                                 for (let i=1 ; i < purposes.length ; i++) {
-                                    target_purpose = target_purpose + '・' + dam_purpose[target_dam['ksj:purpose'][i] - 1]
-                                }
+                                    target_purpose = target_purpose + '・' + dam_purpose[target_dam['ksj:purpose'][i] - 1];
+                                };
                             } else {
-                                target_purpose = dam_purpose[target_dam['ksj:purpose'] - 1]
-                            }
+                                target_purpose = dam_purpose[target_dam['ksj:purpose'] - 1];
+                            };
 
-                            let target_address = target_dam['ksj:address']
+                            let target_address = target_dam['ksj:address'];
                             let prefecture = target_address.match(/.{2,3}?[都道府県]/);
 
                             // ヒントを提出
@@ -268,7 +280,8 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                     }, 6000);
                                 }, 6000);
                             }, 1000);
-                        }
+                        };
+                        break;
 
                     // フォロー時のメッセージ
                     case 'follow':
@@ -276,6 +289,7 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                             type: 'text',
                             text: 'フォローありがとう！よろしくね！'
                         });
+                        break;
 
                     // グループ追加時のメッセージ
                     case 'join':
@@ -283,9 +297,9 @@ express.post('/webhook', line.middleware(line_config), (req, res, next) => {
                             type: 'text',
                             text: '招待ありがとう！よろしくね！'
                         });
-
-                }
-
-        }
+                        break;
+                };
+            break;
+        };
     });
 });
